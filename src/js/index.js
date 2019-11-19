d7d4d831be69de32b5d6c2b4926decf3345e952c
nemanja-ma <nemanja.front@gmail.com>
2019-11-19 23:37:45 +0100

    search module

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const res = await axios(`${proxy}https://api.spoonacular.com/recipes/search?${query}`);
-    console.log(res);
-    
+/*global state of the app
+*search object
+*surrent recipe object
+*shoping list object
+*link recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    //get the query from the view
+    const query = 'pasta';
+    if (query) {
+        //new search object and add to state
+        state.search = new Search(query)
+        //prepare ui for results
+        //search for recipes
+        await state.search.getResults();
+        //sender results to UI
+        console.log(state.search.result);
+        
+    }
 }
-getResults();
+
+document.querySelector('.search').addEventListener('submit', e => {
+e.preventDefault();
+controlSearch();
+});
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default 'I am an eported string';
+import axios from 'axios';
+
+class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = 'bfca48a500b64282aa13ed55488e2cb1';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const res = await axios(`${proxy}https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${this.query}&number=10`);
+            this.result = res.data.results
+            //console.log(this.result);
+        } catch(error) {
+            alert(error)
+        }
+    }
+}
+
+export default Search;
